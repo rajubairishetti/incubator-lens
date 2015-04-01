861353dfa8396e8956ca012c6827eeca85e02c09
Raju Bairishetti <raju.bairishetti@C1MNG0PXDTY3-2.local>
2015-04-01 18:37:56 +0530

    add profile

--- tools/scripts/generate-site-public.sh
+++ tools/scripts/generate-site-public.sh
@@ -44,9 +44,9 @@ CURR_BRANCH=`git branch | sed -n '/\* /s///p'`
 echo "Running site in current lens branch" $CURR_BRANCH
 mvn clean test -Dtest=TestGenerateConfigDoc || die "Unable to generate config docs"
 mvn install -DskipTests
-mvn site site:stage -Ddependency.locations.enabled=false -Ddependency.details.enabled=false || die "unable to generate site"
+mvn site site:stage -DskipTests -Ddependency.locations.enabled=false -Ddependency.details.enabled=false || die "unable to generate site"
 cd lens-server
-mvn enunciate:docs
+mvn enunciate:docs -Penunciate
 cd ..
 echo "Site gen complete"
 
