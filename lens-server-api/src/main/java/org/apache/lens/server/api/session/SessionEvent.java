db85bcf4639a01df577c0fc08456baf2e70dc05c
Raju Bairishetti <raju.bairishetti@C1MNG0PXDTY3-2.local>
2015-04-07 20:42:15 +0530

    add new classes

---
+++ lens-server-api/src/main/java/org/apache/lens/server/api/session/SessionEvent.java
@@ -0,0 +1,70 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.lens.server.api.session;
+
+import java.util.UUID;
+
+import org.apache.lens.api.LensSessionHandle;
+import org.apache.lens.server.api.events.LensEvent;
+
+import lombok.Getter;
+
+public abstract class SessionEvent extends LensEvent {
+
+  /**
+   * The lens session handle.
+   */
+  @Getter
+  protected final LensSessionHandle sessionHandle;
+
+  /**
+   * The id.
+   */
+  protected final UUID id = UUID.randomUUID();
+
+  /**
+   * Instantiates a new session event
+   *
+   * @param eventTime the event time
+   * @param handle    the lens session handle
+   * @param user      name of user
+   */
+  public SessionEvent(long eventTime, LensSessionHandle handle) {
+    super(eventTime);
+    this.sessionHandle = handle;
+  }
+
+  @Override
+  public String getEventId() {
+    return id.toString();
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see java.lang.Object#toString()
+   */
+  @Override
+  public String toString() {
+    StringBuilder buf = new StringBuilder("SessionEvent: ").append(getClass().getSimpleName()).append(":{id: ")
+        .append(id).append(", session:").append(getSessionHandle());
+    return buf.toString();
+  }
+}
